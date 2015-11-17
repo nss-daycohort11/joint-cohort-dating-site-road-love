9047acd17a1c030b0a304b2cfe2013c0b8f85443
micahp0506 <micahp0506@gmail.com>
2015-11-17 11:04:15 -0600

    Working to get login working correctly

--- javascripts/login.js
+++ javascripts/login.js
@@ -3,9 +3,11 @@ define(function(require) {
   var b = require("bootstrap");
   var h = require("hbs");
   var f = require("firebase");
+
   var adduser = require("adduser")
 console.log("yep");
 
+// Auth with facebook
 $("#facebook").click(function() {
 	console.log("click");
 	var ref = new Firebase("https://roadlove.firebaseio.com/");
@@ -15,8 +17,11 @@ $("#facebook").click(function() {
 	  } else {
 	    console.log("Authenticated successfully with payload:", authData);
 	    console.log(authData.facebook.displayName);
+	    // Getting name from Facebook
 	    var userName = authData.facebook.displayName;
+	    // Getting image from Facebook
 	    var userImage = authData.facebook.profileImageURL;
+	    // Adding name and image to userObject object
 	    var userObject = {
 	    	
 	    	"user_name":userName,
@@ -24,9 +29,13 @@ $("#facebook").click(function() {
 
   		};
   		console.log("userObject", userObject);
+  		// Sending userObject to Firebase via promises
   		adduser(userObject)
   			.then(function(addedUser) {
+  				// Removing log in when promise is successful
   				$('.show').remove();
+  				// Showing additional needed profile information
+  				$('#myModal').modal('show');
   				console.log("success");
   			})
   			.fail(function(error) {	
@@ -36,3 +45,8 @@ $("#facebook").click(function() {
 });
 });
 });
+
+
+
+
+
