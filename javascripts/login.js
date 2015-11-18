20454c9ff1d593c1d83838a48a95f04fe947f1c7
micahp0506 <micahp0506@gmail.com>
2015-11-18 15:56:05 -0600

    Refacturing login page

--- javascripts/login.js
+++ javascripts/login.js
@@ -9,79 +9,159 @@ define(function(require) {
   var userName, userImage;
   var userObject = {};
   var auth = require("auth-storage");
-  
+ 
+  // Checking page load
   console.log("yep");
 
+	// Ref to firebase
+	var ref = new Firebase("https://roadlove.firebaseio.com/");
+
+	
+  	$("#facebook").click(function() {
+	    console.log("click");
+	    var authData = ref.getAuth();
+	    console.log("authData", authData);
+    	// If no authData exists
+    	if (authData === null){ 
+	    	console.log("authData", authData);
+    		// Getting auth from Facebook
+	    	ref.authWithOAuthPopup("facebook", function(error, authData) {
+	    		console.log("authData", authData);
+		    	// Setting uid to newuid
+		    	auth.setUid(authData.uid);
+		    	// Declaring variables for real time snapshot
+		    	var usersFirebase = ref.child("users");
+		        var userExists = false;
+		        var userName = authData.facebook.displayName;
+		        var userImage = authData.facebook.profileImageURL;
+		        var userId = authData.uid;
+
+		        //Using real time snapshot because it kept adding users each time we signed in
+		        usersFirebase.once("value", function(dataSnapshot) {
+		            dataSnapshot.forEach(function(childSnapshot) {
+		                //If facebook uid matches, then user already exits
+		                if (childSnapshot.val().uid === authData.uid) {
+		                    userExists = true;
+		                    alert("Welcome back");
+		                    $('.show').remove();
+		                }
+		            });
+		        	//If doesn't match, then push uid, image, displayname to user in firebase
+		            if (userExists === false) {
+		                usersFirebase.push({
+
+		                    "user_name":userName,
+		          			"user_image": userImage,
+		          			"user_uid": userId
+		                        
+		                });
+		                $('.show').remove();
+		              	console.log("success");
+		              	$('#myModal').modal('show');
+            		}  	
+  				});
+			});
+		} else {
+            auth.setUid(authData.uid);
+            // window.location.assign("home.html");
+            // profileInputFields.profileInputDisplay();
+            console.log("hey");
+            $('.show').remove();
+        }	
+	});
+
+});		
+	// $("#logout").click(function()
+	// 	console.log("click");
+ //  	ref.unauth();
 
 
-  $("#facebook").click(function() {
-    console.log("click");
-    // Ref to firebase
-    var ref = new Firebase("https://roadlove.firebaseio.com/");
-    // Getting auth from Facebook
-    ref.authWithOAuthPopup("facebook", function(error, authData) {
-      if(authData){
-        auth.getUid();
-      }
-      else if (error) {
-        console.log("Login Failed!", error);
-      } else {
-        console.log("Authenticated successfully with payload:", authData);
-        console.log(authData.facebook.displayName);
-        var userName = authData.facebook.displayName;
-        var userImage = authData.facebook.profileImageURL;
-        var userId = authData.uid;
-        auth.setUid(authData.uid);
-        var userObject = {
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+//   console.log("yep");
+
+
+
+//   $("#facebook").click(function() {
+//     console.log("click");
+//     // Ref to firebase
+//     var ref = new Firebase("https://roadlove.firebaseio.com/");
+//     // Getting auth from Facebook
+//     ref.authWithOAuthPopup("facebook", function(error, authData) {
+//       if(authData){
+//         auth.getUid();
+//       }
+//       else if (error) {
+//         console.log("Login Failed!", error);
+//       } else {
+//         console.log("Authenticated successfully with payload:", authData);
+//         console.log(authData.facebook.displayName);
+//         var userName = authData.facebook.displayName;
+//         var userImage = authData.facebook.profileImageURL;
+//         var userId = authData.uid;
+//         auth.setUid(authData.uid);
+//         var userObject = {
           
-          "user_name":userName,
-          "user_image": userImage,
-          "user_uid": userId
+//           "user_name":userName,
+//           "user_image": userImage,
+//           "user_uid": userId
 
-        };
-      }
+//         };
+//       }
 
-    })
-      ref.child("users").on("value", function(snapshot) {
-      var users = snapshot.val();
-      console.log("users", users);
+//     })
+//       ref.child("users").on("value", function(snapshot) {
+//       var users = snapshot.val();
+//       console.log("users", users);
 
-        var userAuth = ref.getAuth();
-        var usersArray = [];
-        for (var key in users) {
-          var userObj = users[key];
-          // console.log("userObj", userObj);
-          userObj.key = key;
-          // console.log("key", userObj.key);
-          usersArray[usersArray.length] =  userObj;
-        console.log("userObj", userObj);
-        console.log("userObj.user_uid", userObj.user_uid);
-            console.log("userAuth.uid", userAuth.uid);
+//         var userAuth = ref.getAuth();
+//         var usersArray = [];
+//         for (var key in users) {
+//           var userObj = users[key];
+//           // console.log("userObj", userObj);
+//           userObj.key = key;
+//           // console.log("key", userObj.key);
+//           usersArray[usersArray.length] =  userObj;
+//         console.log("userObj", userObj);
+//         console.log("userObj.user_uid", userObj.user_uid);
+//             console.log("userAuth.uid", userAuth.uid);
 
 
-          if(userObj.user_uid === userAuth.uid){
-            console.log("userObj.user_uid", userObj.user_uid);
-            console.log("userAuth.uid", userAuth.uid);
-            console.log("user exists");
-            auth.setKey(key);
-          }
-          else{
-            console.log("user does not exist");
-          }
-        }       
-          console.log("userObject", userObject);
-          adduser(userObject)
-            .then(function(addedUser) {
-              $('.show').remove();
-              console.log("success");
-              $('#myModal').modal('show');
-            })
-            .fail(function(error) { 
-              console.log("It errored out", error);
-           });
-     });
-  });
-});
+//           if(userObj.user_uid === userAuth.uid){
+//             console.log("userObj.user_uid", userObj.user_uid);
+//             console.log("userAuth.uid", userAuth.uid);
+//             console.log("user exists");
+//             auth.setKey(key);
+//           }
+//           else{
+//             console.log("user does not exist");
+//           }
+//         }       
+//           console.log("userObject", userObject);
+//           adduser(userObject)
+//             .then(function(addedUser) {
+//               $('.show').remove();
+//               console.log("success");
+//               $('#myModal').modal('show');
+//             })
+//             .fail(function(error) { 
+//               console.log("It errored out", error);
+//            });
+//      });
+//   });
+// });
 
  
 
